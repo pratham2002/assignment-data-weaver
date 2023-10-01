31331837b46dcc2e7b217396035d044527c71bc7
Pratham <agrawalpratham2002@gmail.com>
2023-10-01 21:48:18 +0530

    fixed some css in top bar

--- src/componenets/TopBar.jsx
+++ src/componenets/TopBar.jsx
@@ -2,13 +2,24 @@ import React from "react";
 
 export default function TopBar() {
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <img
-        style={{ height: "15vh", objectFit: "contain" }}
-        src="https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books_23-2149342941.jpg"
-        alt="logo"
-      ></img>
-      <h2>Book Search App</h2>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+      }}
+    >
+      <div
+        className="header-container"
+        style={{ display: "flex", alignItems: "flex-end" }}
+      >
+        <img
+          src="https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books_23-2149342941.jpg"
+          alt="logo"
+        />
+        <h2>Book Search App</h2>
+      </div>
     </div>
   );
 }
